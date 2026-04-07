be38a725d4c1f1c3aeea6ca33fc54465318ba6b5
Matatabi298 <s2110998@u.tsukuba.ac.jp>
2026-04-07 16:05:35 +0900

    Add files via upload

--- index.js
+++ index.js
@@ -11,21 +11,24 @@ const client = new Client({
   intents: [GatewayIntentBits.Guilds]
 });
 
-// 起動時にメッセージ送る（初期状態）
+// 現在借りてる人
+let currentUserId = null;
+
+// 起動時
 client.once('ready', async () => {
   console.log(`ログイン成功: ${client.user.tag}`);
 
-  const channel = await client.channels.fetch('1489681970027040970');
+  const channel = await client.channels.fetch('チャンネルID');
 
   const borrowButton = new ButtonBuilder()
     .setCustomId('borrow')
-    .setLabel('借りた！')
+    .setLabel('借りる')
     .setStyle(ButtonStyle.Primary);
 
   const row = new ActionRowBuilder().addComponents(borrowButton);
 
   await channel.send({
-    content: '鍵を借りましたか？',
+    content: '鍵を借りますか？',
     components: [row]
   });
 });
@@ -34,32 +37,63 @@ client.once('ready', async () => {
 client.on(Events.InteractionCreate, async interaction => {
   if (!interaction.isButton()) return;
 
-  // 借りるボタン
+  // 借りる
   if (interaction.customId === 'borrow') {
-    const returnButton = new ButtonBuilder()
-      .setCustomId('return')
-      .setLabel('返した！')
-      .setStyle(ButtonStyle.Danger);
+    currentUserId = interaction.user.id;
 
-    const row = new ActionRowBuilder().addComponents(returnButton);
+    const row = new ActionRowBuilder().addComponents(
+      new ButtonBuilder()
+        .setCustomId('return')
+        .setLabel('返す')
+        .setStyle(ButtonStyle.Danger),
+      new ButtonBuilder()
+        .setCustomId('swap')
+        .setLabel('交換')
+        .setStyle(ButtonStyle.Secondary)
+    );
 
     await interaction.update({
-      content: `<@${interaction.user.id}> が借りました`,
+      content: `<@${currentUserId}> が鍵を借りています`,
       components: [row]
     });
   }
 
-  // 返すボタン
+  // 交換
+  if (interaction.customId === 'swap') {
+    if (!currentUserId) return;
+
+    currentUserId = interaction.user.id;
+
+    const row = new ActionRowBuilder().addComponents(
+      new ButtonBuilder()
+        .setCustomId('return')
+        .setLabel('返す')
+        .setStyle(ButtonStyle.Danger),
+      new ButtonBuilder()
+        .setCustomId('swap')
+        .setLabel('交換')
+        .setStyle(ButtonStyle.Secondary)
+    );
+
+    await interaction.update({
+      content: `<@${currentUserId}> が鍵を借りています`,
+      components: [row]
+    });
+  }
+
+  // 返す
   if (interaction.customId === 'return') {
+    currentUserId = null;
+
     const borrowButton = new ButtonBuilder()
       .setCustomId('borrow')
-      .setLabel('借りた！')
+      .setLabel('借りる')
       .setStyle(ButtonStyle.Primary);
 
     const row = new ActionRowBuilder().addComponents(borrowButton);
 
     await interaction.update({
-      content: '鍵を借りましたか？',
+      content: '鍵を借りますか？',
       components: [row]
     });
   }
